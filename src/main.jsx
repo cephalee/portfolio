4c77c471e83ccd2e29dfebb93610eabcb0635025
Cephalee <yliesdu594590@gmail.com>
2026-01-16 21:08:20 +0100

    Mise à jour configuration pour déploiement

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Portefolio from './Portfolio.jsx';
+import Portfolio from './Portfolio.jsx';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/portfolio/">
-      <Portefolio />
+    <HashRouter> 
+      <Portfolio />
     </HashRouter>
   </React.StrictMode>
 );
